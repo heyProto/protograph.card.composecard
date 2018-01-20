35a10c5e09234d51ba5471827be78871587c41fd
Gaurav Bhagchandani <gb@pykih.com>
2018-01-20 20:45:25 +0530

    Added react-medium-editor

---
+++ src/js/card.jsx
@@ -0,0 +1,192 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import TimeAgo from 'react-timeago';
+import ReactMarkdown from 'react-markdown';
+import Editor from 'react-medium-editor';
+require('medium-editor/dist/css/medium-editor.css');
+require('medium-editor/dist/css/themes/default.css');
+export default class toCluster extends React.Component {
+
+  constructor(props) {
+    super(props)
+    let stateVar = {
+      fetchingData: true,
+      dataJSON: {},
+      schemaJSON: undefined,
+      optionalConfigJSON: {},
+      optionalConfigSchemaJSON: undefined,
+      languageTexts: undefined,
+      content: undefined,
+      editable: false,
+      text: undefined
+    };
+    
+    if (this.props.dataJSON) {
+      stateVar.fetchingData = false;
+      stateVar.dataJSON = this.props.dataJSON;
+      stateVar.languageTexts = this.getLanguageTexts(this.props.dataJSON.data.language);
+    }
+
+    if(this.props.content){
+      stateVar.content = this.props.content;
+    }
+    if (this.props.optionalConfigJSON) {
+      stateVar.optionalConfigJSON = this.props.optionalConfigJSON;
+    }
+
+    if (this.props.optionalConfigSchemaJSON) {
+      stateVar.optionalConfigSchemaJSON = this.props.optionalConfigSchemaJSON;
+    }
+    if(this.props.editable){
+      stateVar.editable=this.props.editable;
+    }
+    this.state = stateVar;
+    this.processLink = this.processLink.bind(this);
+  }
+
+  exportData() {
+    return this.props.selector.getBoundingClientRect();
+  }
+  handleEditorChange(e) {
+    console.log(e.target.getContent());
+  }
+  componentDidMount() {
+    if (this.state.fetchingData) {
+      axios.all([
+        axios.get(this.props.dataURL),
+        axios.get(this.props.optionalConfigURL),
+        axios.get(this.props.optionalConfigSchemaURL)
+      ])
+      .then(axios.spread((card, opt_config, opt_config_schema) => {
+        this.setState({
+          fetchingData: false,
+          dataJSON: card.data,
+          optionalConfigJSON: opt_config.data,
+          optionalConfigSchemaJSON: opt_config_schema.data,
+          languageTexts: this.getLanguageTexts(card.data.data.language)
+        });
+      }));
+    } else {
+      this.componentDidUpdate();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.dataJSON) {
+      this.setState({
+        content: nextProps.content
+      });
+    }
+  }
+
+  getLanguageTexts(languageConfig) {
+    let language = languageConfig ? languageConfig : "hindi",
+      text_obj;
+
+    switch(language.toLowerCase()) {
+      case "hindi":
+        text_obj = {
+          font: "'Sarala', sans-serif"
+        }
+        break;
+      default:
+        text_obj = {
+          font: undefined
+        }
+        break;
+    }
+
+    return text_obj;
+  }
+  handleChange(data){
+    this.setState({
+      content:data
+    })
+  }
+  componentDidUpdate() {
+    let data = this.state.dataJSON.data;
+  }
+  renderCol7() {
+    let data = this.state.dataJSON.data;
+    if (this.state.fetchingData ){
+      return(<div>Loading</div>)
+    } else {
+        return (
+          <div className="protograph-col7-mode">
+            <Editor
+            tag="pre"
+            text={this.state.text}
+            onChange={(e)=>{this.handleChange(e)}}
+            options={{disableEditing:!this.state.editable,toolbar: !this.state.editable ? false :{buttons: [
+              'bold',
+              'italic',
+              'h1','h2','h3',
+              'justifyCenter',
+              'quote',
+              'anchor'
+          ]}}}
+          />
+          </div>
+        )
+    }
+  }
+  renderCol4() {
+    let data = this.state.dataJSON.data;
+    if (this.state.fetchingData ){
+      return(<div>Loading</div>)
+    } else {
+        return (
+          <div className="protograph-col4-mode">
+            <Editor
+            tag="pre"
+            text={this.state.text}
+            onChange={(e)=>{this.handleChange(e)}}
+            options={{disableEditing:!this.state.editable,toolbar: !this.state.editable ? false :{buttons: [
+              'bold',
+              'italic',
+              'h1','h2','h3',
+              'justifyCenter',
+              'quote',
+              'anchor'
+          ]}}}
+          />
+          </div>
+        )
+    }
+  }
+  processLink(e) {
+    const links = this.state.dataJSON.data.links;
+    switch (e.type) {
+      case 'linkReference':
+        let linkRef = +e.identifier;
+        e.type = "link";
+        e.title = null;
+        if ((linkRef - 1) < links.length ) {
+          e.url = this.state.dataJSON.data.links[+e.identifier - 1].link;
+          return true;
+        } else  {
+          e.type = "span"
+          return true;
+        }
+        break;
+      // Don't allow any external link. Make all the links to span.
+      case 'link':
+        e.type = "span"
+        return true;
+      default:
+        return true;
+    }
+  }
+
+  render() {
+    switch(this.props.mode) {
+      case 'col7' :
+        return this.renderCol7();
+        break;
+      case 'col4':
+        return this.renderCol4();
+        break;
+    }
+  }
+}
---
+++ src/js/edit_card.jsx
@@ -0,0 +1,334 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Card from './card.jsx';
+import JSONSchemaForm from '../../lib/js/react-jsonschema-form';
+import TinyMCE from 'react-tinymce';
+
+export default class editToCluster extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      step: 1,
+      dataJSON: {},
+      mode: "col7",
+      publishing: false,
+      schemaJSON: undefined,
+      fetchingData: true,
+      optionalConfigJSON: {},
+      optionalConfigSchemaJSON: undefined,
+      uiSchemaJSON: {},
+      refLinkDetails: undefined,
+      content: undefined
+    }
+    this.refLinkSourcesURL = window.ref_link_sources_url
+    this.toggleMode = this.toggleMode.bind(this);
+    this.formValidator = this.formValidator.bind(this);
+  }
+
+  exportData() {
+    let getDataObj = {
+      step: this.state.step,
+      dataJSON: this.state.dataJSON,
+      schemaJSON: this.state.schemaJSON,
+      optionalConfigJSON: this.state.optionalConfigJSON,
+      optionalConfigSchemaJSON: this.state.optionalConfigSchemaJSON
+    }
+    getDataObj["name"] = getDataObj.dataJSON.data.title.substr(0,225); // Reduces the name to ensure the slug does not get too long
+    return getDataObj;
+  }
+  handleEditorChange(e) {
+    console.log(e.target.getContent());
+    this.setState({
+      content: e.target.getContent()
+    })
+  }
+  componentDidMount() {
+    // get sample json data based on type i.e string or object.
+    if (this.state.fetchingData){
+      axios.all([
+        axios.get(this.props.dataURL),
+        axios.get(this.props.schemaURL),
+        axios.get(this.props.optionalConfigURL),
+        axios.get(this.props.optionalConfigSchemaURL),
+        axios.get(this.props.uiSchemaURL),
+        axios.get(this.refLinkSourcesURL)
+      ])
+      .then(axios.spread((card, schema, opt_config, opt_config_schema, uiSchema, linkSources) => {
+        let stateVars = {
+          fetchingData: false,
+          dataJSON: card.data,
+          schemaJSON: schema.data,
+          optionalConfigJSON: opt_config.data,
+          optionalConfigSchemaJSON: opt_config_schema.data,
+          uiSchemaJSON: uiSchema.data,
+          refLinkDetails: linkSources.data
+        },
+        links = stateVars.dataJSON.data.links;
+
+        // var  e = new Date
+        //   ,n = e.getDate()
+        //   , r = e.getMonth() + 1
+        //   , o = e.getFullYear(),
+        //   hours = e.getHours(),
+        //   min = e.getMinutes();
+        // var date =  `${o}-${r}-${n} ${hours}:${min}`
+        stateVars.dataJSON.data.published_date = stateVars.dataJSON.data.published_date || (new Date).toString()
+
+        if (links.length) {
+          this.checkAndUpdateLinkInfo(links, stateVars.refLinkDetails);
+        }
+
+        this.setState(stateVars);
+      }));
+    }
+  }
+
+  checkAndUpdateLinkInfo(links, refLinkDetails) {
+    links.forEach((e,i) => {
+      let linkDetails = this.lookUpLinkDetail(e.link, refLinkDetails);
+      if (linkDetails) {
+        e.favicon_url = linkDetails.favicon_url;
+        e.publication_name = linkDetails.name;
+      }
+    });
+  }
+
+  lookUpLinkDetail(link, refLinkDetails) {
+    refLinkDetails = refLinkDetails || this.state.refLinkDetails;
+
+    let linkParams = this.parseUrl(link),
+      lookupLink = refLinkDetails.filter((e, i) => {
+        return e.url === linkParams.origin;
+      })[0];
+
+      return lookupLink;
+  }
+
+  parseUrl(url) {
+    var parser = document.createElement('a'),
+      search;
+    parser.href = url;
+    return {
+      protocol: parser.protocol,
+      host: parser.host,
+      hostnam: parser.hostname,
+      port: parser.port,
+      pathname: parser.pathname,
+      hash: parser.hash,
+      searchString: parser.search,
+      origin: parser.origin
+    };
+  }
+
+  onChangeHandler({formData}) {
+    switch (this.state.step) {
+      case 1:
+        this.setState((prevStep, prop) => {
+          let dataJSON = prevStep.dataJSON;
+          this.checkAndUpdateLinkInfo(formData.links)
+          dataJSON.data = formData;
+          console.log(formData.published_date, ".......")
+          return {
+            dataJSON: dataJSON
+          }
+        })
+        break;
+      case 2:
+        this.setState((prevState, prop) => {
+          let dataJSON = prevState.dataJSON;
+          if (formData.analysis && formData.analysis.length > 0) {
+            dataJSON.data.analysis = formData.analysis;
+          } else {
+            delete dataJSON.data.analysis;
+          }
+
+          return {
+            dataJSON: dataJSON
+          }
+        })
+        break;
+    }
+  }
+
+  onSubmitHandler({formData}) {
+    switch(this.state.step) {
+      case 1:
+        this.setState({ step: 2 });
+        break;
+      case 2:
+        if (typeof this.props.onPublishCallback === "function") {
+          this.setState({ publishing: true });
+          let publishCallback = this.props.onPublishCallback();
+          publishCallback.then((message) => {
+            this.setState({ publishing: false });
+          });
+        }
+        break;
+    }
+  }
+
+  formValidator(formData, errors) {
+    switch (this.state.step) {
+      case 1:
+        formData.links.forEach((e, i) => {
+          let details = this.lookUpLinkDetail(e.link);
+          if (!details) {
+            errors.links[i].addError("Story domain is invalid");
+          }
+        });
+        return errors;
+      default:
+        return errors;
+    }
+    return errors;
+  }
+
+  renderSEO() {
+    let d = this.state.dataJSON.data,
+      linksHTML = "<ul>";
+    d.links.forEach(e => {
+      linksHTML += `<li><a href="${e.link}" target="_blank">${e.publication_name}</a></li>`
+    })
+    linksHTML += "</ul>";
+    let blockquote_string = `<h1>${d.title}</h1><p>${d.by_line}</p><p>${d.published_date}</p>${linksHTML}`;
+    let seo_blockquote = '<blockquote>' + blockquote_string + '</blockquote>'
+    return seo_blockquote;
+  }
+
+  renderSchemaJSON() {
+    let schema;
+    switch(this.state.step){
+      case 1:
+        schema = JSON.parse(JSON.stringify(this.state.schemaJSON.properties.data))
+        delete schema.properties.analysis;
+        return schema;
+        break;
+      case 2:
+        schema = {
+          properties: {
+            analysis: this.state.schemaJSON.properties.data.properties.analysis
+          },
+          "type": "object",
+        }
+        return schema;
+        break;
+    }
+  }
+
+  renderFormData() {
+    switch(this.state.step) {
+      case 1:
+        return this.state.dataJSON.data;
+        break;
+      case 2:
+        return {analysis: this.state.dataJSON.data.analysis};
+        break;
+    }
+  }
+
+  showLinkText() {
+    switch(this.state.step) {
+      case 1:
+        return '';
+        break;
+      case 2:
+        return '< Back';
+        break;
+    }
+  }
+
+  showButtonText() {
+    switch(this.state.step) {
+      case 1:
+        return 'Next';
+        break;
+      case 2:
+        return 'Publish';
+        break;
+    }
+  }
+
+  getUISchemaJSON() {
+    switch (this.state.step) {
+      case 1:
+        return this.state.uiSchemaJSON.section1.data;
+        break;
+      case 2:
+        return this.state.uiSchemaJSON.section2.data;
+        break;
+      default:
+        return {};
+        break;
+    }
+  }
+
+  onPrevHandler() {
+    let prev_step = --this.state.step;
+    this.setState({
+      step: prev_step
+    });
+  }
+
+  toggleMode(e) {
+    let element = e.target.closest('a'),
+      mode = element.getAttribute('data-mode');
+
+    this.setState((prevState, props) => {
+      let newMode;
+      if (mode !== prevState.mode) {
+        newMode = mode;
+      } else {
+        newMode = prevState.mode
+      }
+
+      return {
+        mode: newMode
+      }
+    })
+  }
+
+  render() {
+    if (this.state.fetchingData) {
+      return(<div>Loading</div>)
+    } else {
+      return (
+        <div className="proto-container">
+          <div className="ui grid form-layout">
+            <div className="row">
+              <div className="sixteen wide column proto-card-preview proto-share-card-div">
+                <div className="protograph-menu-container">
+                  <div className="ui compact menu">
+                    <a className={`item ${this.state.mode === 'col7' ? 'active' : ''}`}
+                      data-mode='col7'
+                      onClick={this.toggleMode}
+                    >
+                      col-7
+                    </a>
+                    <a className={`item ${this.state.mode === 'col4' ? 'active' : ''}`}
+                      data-mode='col4'
+                      onClick={this.toggleMode}
+                    >
+                      col-4
+                    </a>
+                  </div>
+                </div>
+                <div className="protograph-app-holder">
+                  <Card
+                    mode={this.state.mode}
+                    dataJSON={this.state.dataJSON}
+                    schemaJSON={this.state.schemaJSON}
+                    editable={true}
+                    optionalConfigJSON={this.state.optionalConfigJSON}
+                    optionalConfigSchemaJSON={this.state.optionalConfigSchemaJSON}
+                    content={this.state.content}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+}
